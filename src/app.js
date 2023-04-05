756b56d033072f2f9869001217d266f624476b63
augusto <augustoogas7@gmail.com>
2023-04-04 23:22:01 -0300

    empezamos a definir app.js, falta controllers y routes, investigue mejor y se como funcione use y demas

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log("Hola");
+//Importamos libreria de express
+
+const express = require('express');
+
+//ejecutamos la funcion
+const app = express();
+
+//configuramos el puerto ,el cual pregunta si ya tiene uno creado y si no que sea el 300
+const PORT = process.env.PORT || 3000;
+
+
+//iMPORTAMOS la ruta donde va a estar nuestro funcionamiento de la app
+const docentesRouter = require('./routes/docentes.routes');
+
+
+//cargamos la funcion de middleware
+app.use(express.json())
+app.use('/alumnos',docentesRouter.router);
+
+//app.LISTEN pone a escuchar al servidor en el puerto 3000
+app.listen(PORT, ()=>{console.log(`App lista escuhando en el puerto ${PORT}`)} );
